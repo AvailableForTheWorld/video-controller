78709b9e3eecf612048362738690827bbcf0b27a
ykk <w2659124707@126.com>
2022-09-04 22:06:45 +0800

    完善了otions

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("#config").addEventListener("click", function () {
-    window.open(chrome.runtime.getURL("options.html"));
+    // window.open("../option/options.html");
+    chrome.runtime.openOptionsPage()
   });
 
   document.querySelector("#about").addEventListener("click", function () {
